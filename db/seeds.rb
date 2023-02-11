0ee4567077b22bb13acf056ff9e7a7697e4af0c5
Edsey-omenda <omendajames0317@gmail.com>
2023-02-11 23:33:14 +0300

    updates seeds and migrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,10 @@
 Customer.destroy_all
 Account.destroy_all
 
-customer1 = Customer.create(name: "James Omenda", account_type: "Master card", account_number: 11218031720132016, email: "omendajames0317@gmail.com", age: 24, contact: "Telephone +254700332508")
-customer2 = Customer.create(name: "Harvey Spector", account_type: "Visa card", account_number: 20111217192021239, email: "reginaldspector@gmail.com", age: 38, contact: "Telephone +111205450")
-customer3 = Customer.create(name: "Rachel Zane", account_type: "Master card", account_number: 14589023176009417, email: "rachelezabeth@gmail.com", age: 28, contact: "Telephone +254769334509")
-customer4 = Customer.create(name: "Lauren Akinyi", account_type: "Rupay card", account_number: 20171819222523119, email: "akinyilosh@gmail.com", age: 25, contact: "Telephone +254712097658")
+customer1 = Customer.create(name: "James Omenda", account_type: "Master card", account_number: 11218031720132016, email: "omendajames0317@gmail.com", age: 24, contact:  +254700332508)
+customer2 = Customer.create(name: "Harvey Spector", account_type: "Visa card", account_number: 20111217192021239, email: "reginaldspector@gmail.com", age: 38, contact: +111205450)
+customer3 = Customer.create(name: "Rachel Zane", account_type: "Master card", account_number: 14589023176009417, email: "rachelezabeth@gmail.com", age: 28, contact:  +254769334509)
+customer4 = Customer.create(name: "Lauren Akinyi", account_type: "Rupay card", account_number: 20171819222523119, email: "akinyilosh@gmail.com", age: 25, contact:  +254712097658)
 
 account1 = Account.create(account_number: 11218031720132016, customer_name: "James Omenda", debit_Cnumber: 201721, credit_Cnumber: 2018190, account_type: "Master card", amount: 128000, customer: customer1)
 account2 = Account.create(account_number: 20111217192021239, customer_name: "Harvey Spector", debit_Cnumber: 788900, credit_Cnumber: 203479, account_type: "Visa card", amount: 788000, customer: customer2)
